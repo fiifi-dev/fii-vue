b8a34172b6341e3dee438677edff3c7f170fad95
Dickson Afful <fiifi.dev@gmail.com>
2022-07-30 12:32:33 +0000

    add admin page

---
+++ src/components/layouts/FAdminPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="w-full h-full flex items-center justify-center">
+    <div :style="styles">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { StyleValue } from "vue";
+
+export default defineComponent({
+  name: "FAdminPage",
+
+  props: {
+    width: {
+      type: String,
+      required: false,
+      default: "10rem",
+      validator: (value: string) => {
+        const test = new RegExp("^\\d+(px|em|rem)$").test(value);
+        if (!test)
+          console.error("Must provide a valid size: 10rem | 10px | 10em");
+        return test;
+      },
+    },
+  },
+  setup(props) {
+    const styles = computed<StyleValue>(() => ({
+      width: props.width,
+    }));
+
+    return { styles };
+  },
+});
+</script>
+
+<style scoped></style>
