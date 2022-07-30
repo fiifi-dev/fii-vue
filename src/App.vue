278fedb70b0640aa6f569ae4eb1813c281720d86
Dickson Afful <fiifi.dev@gmail.com>
2022-07-30 13:26:06 +0000

    fix: button

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <FAdminLayout>
-    <FAdminPage> sdfsdf </FAdminPage>
+    <FDataTable :headers="headers" show-border @create="handle()" />
   </FAdminLayout>
 </template>
 
 <script setup lang="ts">
 import FTestMenu from "./components/menu/FDropdown.vue";
-import { DropdownItem } from "./types";
+import { DropdownItem, TableHeader } from "./types";
 
-const items: DropdownItem[] = [
-  { key: "create", label: "Create" },
-  { key: "update", label: "Update" },
+const headers: TableHeader[] = [
+  { key: "full_name", label: "Full Name" },
+  { key: "username", label: "Username" },
+  { key: "email", label: "Email" },
+  { key: "role_label", label: "Role" },
 ];
+
+const handle = () => {
+  console.log("hello world");
+};
 </script>
 
 <style scoped></style>
