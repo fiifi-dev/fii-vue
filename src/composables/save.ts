28170d1c7fff4101491a75cafa54aaeb4c34ada8
Dickson Afful <fiifi.dev@gmail.com>
2022-07-31 10:50:14 +0000

    chore: add save composable [publish]

--- src/composables/save.ts
+++ src/composables/save.ts
@@ -2,25 +2,25 @@ import { ref } from "vue";
 import { useRoute, useRouter, type RouteLocationRaw } from "vue-router";
 
 export const useIsUpdate = () => {
-  const isUpdate = ref(false);
-  const id = ref("");
-  const route = useRoute();
+    const isUpdate = ref(false);
+    const id = ref("");
+    const route = useRoute();
 
-  if (route.params.id === undefined) {
-    isUpdate.value = false;
-    id.value = "";
-  } else {
-    isUpdate.value = true;
-    id.value = route.params.id as string;
-  }
+    if (route.params.id === undefined) {
+        isUpdate.value = false;
+        id.value = "";
+    } else {
+        isUpdate.value = true;
+        id.value = route.params.id as string;
+    }
 
-  return { isUpdate, id };
+    return { isUpdate, id };
 };
 
 export const useGoTo = (indexRoute: RouteLocationRaw) => {
-  const router = useRouter();
+    const router = useRouter();
 
-  return () => {
-    router.push(indexRoute);
-  };
+    return () => {
+        router.push(indexRoute);
+    };
 };
