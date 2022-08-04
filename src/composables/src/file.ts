5f0cfb1de93336f69d1b6629fe6e3a5ec6b3267d
Dickson Afful <fiifi.dev@gmail.com>
2022-08-04 20:29:25 +0000

    fix: has file composable [publish]

--- src/composables/src/file.ts
+++ src/composables/src/file.ts
@@ -1,5 +1,8 @@
 import type { Ref } from "vue";
 
-export const useHasFile = <T = unknown>(file: Ref<T>, url: () => string) => {
+export const useHasFile = <T = unknown>(
+  file: Ref<T>,
+  url: () => string | undefined
+) => {
   return computed(() => !file.value || !url());
 };
